efb2d2ff801a7a4db571b37f47eca2ed0b503687
Scott McKean <smckean@geoengio.com>
2020-01-14 17:37:55 -0700

    Add docstrings to functions

--- code/helper_functions.py
+++ code/helper_functions.py
@@ -1,6 +1,12 @@
-# define a tiny atomic function
-def plot_top_n(well_data_df, top_k_index = pd.Int64Index([1]),  
-               target_index = pd.Int64Index([1]), title = None):
+def plot_top_k(well_data_df, top_k_index = pd.Int64Index([1]),  
+               target_index = pd.Int64Index([1])):
+    """Highlight the top k wells with matplotlib.
+
+    Keyword arguments:
+    well_data_df -- the dataframe with the well data (black)
+    top_k_index -- the index for the top k wells (red)
+    target_index -- the index for the target well (blue)
+    """
     fig, ax = plt.subplots(figsize = (8,7))
     ax.scatter(well_data_df.loc[target_index,:].mean_ss_easting,
                well_data_df.loc[target_index,:].mean_ss_northing, 
@@ -11,19 +17,30 @@ def plot_top_n(well_data_df, top_k_index = pd.Int64Index([1]),
     ax.scatter(well_data_df.drop(top_k_index).mean_ss_easting,
                well_data_df.drop(top_k_index).mean_ss_northing, 
                zorder=1, alpha= 0.5, c='k', s=10)
-    ax.set_title(title)
+    ax.set_title('Scatter Plot of Top' + k + 'Wells')
     
 # make a kNN predict function
-def predict_print_knn(well_data_df, dist_func, k , target_index, column_str, title_str = ""):
+def predict_print_knn(well_data_df, dist_func, k , target_index, target_column):
+    """Predict and print a target column using the knn method.
+
+    Keyword arguments:
+    well_data_df -- the dataframe with the well data (black)
+    dist_func -- a scipy.spatial.distance function
+    k -- the number of neighbours
+    target_index -- row index of the well we are targetting
+    target_column -- column of the target variable
+    """
     well_data_df = well_data_df.assign(distance = well_data_df.apply(
         dist_func, axis=1, v=well_data_df.loc[target_index,:]))
     
     top_k_index = well_data_df.nsmallest(k,'distance').index
-    plot_top_n(well_data_df, top_k_index, target_index, title_str)
+    
+    # note the nested functions
+    plot_top_n(well_data_df, top_k_index, target_index)
     
     # let's 'predict' the breakdown_isip_ratio - kinda a fluke
-    pred = well_data_df.loc[top_k_index,column_str].mean()
-    actual = well_data_df.loc[target_index,column_str]
+    pred = well_data_df.loc[top_k_index,target_column].mean()
+    actual = well_data_df.loc[target_index,target_column]
     
     # print using f-strings
     print(title_str + ' kNN prediction: ' 
@@ -33,6 +50,12 @@ def predict_print_knn(well_data_df, dist_func, k , target_index, column_str, tit
 
 # simple 2D colour plot
 def plot_single_map(df, xyz_cols = ['x','y','first_order_residual']):
+    """Plot a single map.
+
+    Keyword arguments:
+    df -- the dataframe with the well data 
+    xyz_cols -- a vector of the x,y, and target column
+    """
     fig, ax = plt.subplots(figsize = (5.5,5))
     ax.set_title(xyz_cols[2])
     ax.tripcolor(df[xyz_cols[0]], df[xyz_cols[1]], df[xyz_cols[2]])
@@ -40,8 +63,13 @@ def plot_single_map(df, xyz_cols = ['x','y','first_order_residual']):
     plt.tight_layout()
     plt.show()
 
-# TODO: MAke this function to plot all plots...
 def plot_four_maps(df):
+    """Plot a four maps based on index location, with 0 being an index
+    column, 1=x, 2=y and 3=z.
+
+    Keyword arguments:
+    df -- the dataframe with the well data
+    """
     plt.figure(figsize=(8, 8))
     G = gridspec.GridSpec(2, 2)
     ax1 = plt.subplot(G[0, 0])
@@ -69,6 +97,11 @@ def plot_four_maps(df):
     plt.show()
     
 def plot_pca_variance(pca):
+    """Line plot of the pca variance ratio - individual and cummulative 
+
+    Keyword arguments:
+    pca -- pca object
+    """
     plt.figure(figsize=(8, 5))
     G = gridspec.GridSpec(1, 2)
     ax1 = plt.subplot(G[0, 0])
@@ -87,6 +120,12 @@ def plot_pca_variance(pca):
     plt.show()
        
 def plot_pca_unit_circle(pca, df):
+    """Plot pca correlation circle for first two PCAs
+
+    Keyword arguments:
+    pca -- pca object
+    df -- the dataframe with the well data
+    """
     coefficients = np.transpose(pca.components_)
     
     ex_var_ratio = pca.explained_variance_ratio_
@@ -96,7 +135,6 @@ def plot_pca_unit_circle(pca, df):
     pca_info = pd.DataFrame(coefficients, 
                             columns=pca_cols, 
                             index=df.columns)
-    
     
     plt.figure(figsize=(5.5, 5))
     plt.Circle((0,0),radius=10, color='g', fill=False)
@@ -118,6 +156,13 @@ def plot_pca_unit_circle(pca, df):
     plt.show()
     
 def plot_cluster_results(clust_res, df):
+    """Scatter plot of cluster results, with xy of dataframe and
+    cluster centres.
+
+    Keyword arguments:
+    clust_res -- the cluster result - should have cluster_centers_, labels_
+    df -- the dataframe with the well data
+    """
     plt.figure(figsize=(8, 5))
     G = gridspec.GridSpec(1, 2)
     ax1 = plt.subplot(G[0, 0])
@@ -144,6 +189,14 @@ def plot_cluster_results(clust_res, df):
     plt.show()
     
 def kmeans_elbowplot(df, kmax = 12):
+    """Iterate the k-means algorith and produce an elbow plot to optimize 
+    the number of clusters using the inertia_ attribute and the two sklearn
+    unsupervised scoring metrics (silhouette and calinski harabaz)
+
+    Keyword arguments:
+    df -- the dataframe with the well data
+    kmax -- the maximum number of clusters
+    """
     wcss = []
     silhouette = []
     calinski_harabaz = []
@@ -156,7 +209,7 @@ def kmeans_elbowplot(df, kmax = 12):
         silhouette.append(sil_score)
         calinski_harabaz.append(ch_score)
     
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize=(8, 5))
     G = gridspec.GridSpec(1, 3)
     ax1 = plt.subplot(G[0, 0])
     ax2 = plt.subplot(G[0, 1])
@@ -178,6 +231,12 @@ def kmeans_elbowplot(df, kmax = 12):
     plt.show()
     
 def plot_dendrogram(df, method = 'ward'):
+    """Plot the dendrogram for heirarchical clustering
+
+    Keyword arguments:
+    df -- the dataframe with the well data
+    method -- agglomoerative method to calculate the linkage matrix
+    """
     linkage_matrix = linkage(df, method)
     figure = plt.figure(figsize=(8, 8))
     dendrogram(
